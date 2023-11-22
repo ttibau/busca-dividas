82e065a96ec414ed1db6e8bca52baabe95518df3
Eduardo Pedrosa <epedrosa@dxainvest.com>
2023-11-22 16:08:50 -0300

    finalizando layout do preview das informações free

--- app/free-result/components/FreeResultComponent.tsx
+++ app/free-result/components/FreeResultComponent.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import result from '@/shared/exemple-result.json';
+import '../style/style.css';
 
 function FreeResultComponent() {
   function handleShowValue(value: string) {
@@ -13,111 +14,213 @@ function FreeResultComponent() {
   return (
     <div className="min-h-screen bg-base-300 pt-[220px]">
       <div className="flex flex-col items-center pl-5 pr-5">
-        <div className="bg-base-100 max-w-max w-full rounded-lg p-8 ml-12 mr-12 shadow-lg">
-          <div className="flex flex-col md:flex-row gap-8 justify-between">
-            <div>
-              <h2 className="text-3xl font-semibold text-gray-800 text-center">
-                Resultado para o CPF/CNPJ:
-              </h2>
-              <div className="flex flex-col items-center mt-8">
-                <input
-                  value={result.sintese.reg.cpf}
-                  type="text"
-                  placeholder="CPF/CNPJ"
-                  className="input text-center input-bordered input-lg w-full max-w-xs bg-gray-200 text-black text-2xl border-[2px] h-[72px]"
-                />
+        <div className="bg-base-100 w-full rounded-lg p-8 ml-12 mr-12 shadow-lg mb-12">
+          <div className="flex flex-col md:flex-row gap-0 md:gap-5 items-center">
+            <h2 className="text-2xl font-semibold text-gray-800 text-center mt-8">
+              Resultado para o CPF/CNPJ:
+            </h2>
+            <div className="flex flex-col items-center mt-8">
+              <span className="bg-gray-200 shadow-lg text-2xl p-2 rounded-lg">
+                {result.sintese.reg.cpf}
+              </span>
+            </div>
+          </div>
+          <div className="flex flex-col lg:flex-row gap-20 mt-12">
+            <div className="overflow-x-auto mt-8">
+              <span className="block text-xl font-semibold ml-2 mb-5 bg-base-200 max-w-max p-2 rounded-lg shadow-lg">
+                - Dados de registro:
+              </span>
+              <table className="table">
+                {/* head */}
+                <thead>
+                  <tr>
+                    <th></th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {/* row 1 */}
+                  <tr>
+                    <td>Nome:</td>
+                    <td>{handleShowValue(result.sintese.reg.nome)}</td>
+                  </tr>
+                  <tr>
+                    <td>CPF:</td>
+                    <td>{handleShowValue(result.sintese.reg.cpf)}</td>
+                  </tr>
+                  <tr>
+                    <td>Primeiro nome:</td>
+                    <td>{handleShowValue(result.sintese.reg.primeiro_nome)}</td>
+                  </tr>
+                  <tr>
+                    <td>Nome do meio:</td>
+                    <td>{handleShowValue(result.sintese.reg.nome_meio)}</td>
+                  </tr>
+                  <tr>
+                    <td>Ultimo nome:</td>
+                    <td>{handleShowValue(result.sintese.reg.ultimo_nome)}</td>
+                  </tr>
+                  <tr>
+                    <td>Gênero:</td>
+                    <td>{handleShowValue(result.sintese.reg.sexo)}</td>
+                  </tr>
+                  <tr>
+                    <td>Data de nascimento:</td>
+                    <td>{handleShowValue(result.sintese.reg.nascimento)}</td>
+                  </tr>
+                  <tr>
+                    <td>Nome da mãe:</td>
+                    <td>{handleShowValue(result.sintese.reg.mae)}</td>
+                  </tr>
+                  <tr>
+                    <td>Nome do pai:</td>
+                    <td>{handleShowValue(result.sintese.reg.pai)}</td>
+                  </tr>
+                  <tr>
+                    <td>Estado civil:</td>
+                    <td>{handleShowValue(result.sintese.reg.estado_civil)}</td>
+                  </tr>
+                  <tr>
+                    <td>RG:</td>
+                    <td>{handleShowValue(result.sintese.reg.rg)}</td>
+                  </tr>
+                  <tr>
+                    <td>Nacionalidade:</td>
+                    <td>{handleShowValue(result.sintese.reg.nacionalidade)}</td>
+                  </tr>
+                  <tr>
+                    <td>Titulo de eleitor:</td>
+                    <td>
+                      {handleShowValue(result.sintese.reg.titulo_eleitor)}
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div className="overflow-x-auto">
+              <span className="block text-xl font-semibold ml-2 mb-5 bg-base-200 max-w-max p-2 rounded-lg shadow-lg mt-8">
+                - Dados de contato:
+              </span>
+              <div className="mt-8">
+                {/* telefone */}
+                <div>
+                  <span className="block text-lg font-semibold mb-2">
+                    Telefone(s):
+                  </span>
+                  <div className="flex flex-col gap-2">
+                    {result.telefones.registro.map((tel, i: number) => {
+                      return (
+                        <span key={i} className="block">
+                          {`(${tel.ddd}) ${tel.telefone}`}{' '}
+                        </span>
+                      );
+                    })}
+                  </div>
+                </div>
+                {/* emails */}
+                <div className="mt-5">
+                  <span className="block text-lg font-semibold mb-2">
+                    Email(s):
+                  </span>
+                  <div className="flex flex-col gap-2">
+                    {result.emails.registro.map((email, i: number) => {
+                      return (
+                        <span key={i} className="block">
+                          {`${email.email}`}
+                        </span>
+                      );
+                    })}
+                  </div>
+                </div>
+                {/* irmãos */}
+                <div>
+                  <span className="block text-xl font-semibold ml-2 mb-5 bg-base-200 max-w-max p-2 rounded-lg shadow-lg mt-8">
+                    - Irmão(s):
+                  </span>
+                  <div className="flex flex-col gap-2">
+                    {result.parentes.irmaos.registro.map((irmao, i: number) => {
+                      return (
+                        <div key={i}>
+                          <span className="">{`${irmao.nome} - `}</span>
+                          <span className="font-semibold">{`Nascido em ${irmao.nascimento}`}</span>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
               </div>
             </div>
-            <ul className="grid grid-cols-[1fr] md:grid-cols-[1fr_1fr] lg:grid-cols-[1fr_1fr_1fr] gap-5">
-              <li>
-                <span className="block w-full bg-primary text-white text-lg text-center rounded-t-lg p-2">
-                  Nome
+            <div className="overflow-x-auto">
+              <div>
+                <span className="block text-xl font-semibold ml-2 mb-5 bg-base-200 max-w-max p-2 rounded-lg shadow-lg mt-8">
+                  - Endereço(s):
                 </span>
-                <span className="block w-full bg-base-200 shadow-lg py-4 px-2 text-center">
-                  {handleShowValue(result.sintese.reg.nome)}
+                <div>
+                  {result.enderecos.registro.map((address, i: number) => {
+                    return (
+                      <div key={i}>
+                        <span className="block text-lg font-medium mb-4 mt-4">
+                          Endereço: {i + 1}
+                        </span>
+                        <ul>
+                          <li>
+                            <span className="font-bold text-sm">
+                              Endereço:{' '}
+                            </span>
+                            <span className="text-sm">
+                              {address.endCompleto}
+                            </span>
+                          </li>
+                          <li>
+                            <span className="font-bold text-sm">Bairro: </span>
+                            <span className="text-sm">{address.bairro}</span>
+                          </li>
+                          <li>
+                            <span className="font-bold text-sm">Cidade: </span>
+                            <span className="text-sm">{address.cidade}</span>
+                          </li>
+                          <li>
+                            <span className="font-bold text-sm">Número: </span>
+                            <span className="text-sm">{address.numero}</span>
+                          </li>
+                          <li>
+                            <span className="font-bold text-sm">
+                              Complemento:{' '}
+                            </span>
+                            <span className="text-sm">{address.compl}</span>
+                          </li>
+                          <li>
+                            <span className="font-bold text-sm">CEP: </span>
+                            <span className="text-sm">{address.cep}</span>
+                          </li>
+                          <li>
+                            <span className="font-bold text-sm">UF: </span>
+                            <span className="text-sm">{address.uf}</span>
+                          </li>
+                        </ul>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+              <div>
+                <span className="block text-xl font-semibold ml-2 mb-5 bg-base-200 max-w-max p-2 rounded-lg shadow-lg mt-8">
+                  - Filho(s):
                 </span>
-              </li>
-              <li>
-                <span className="block w-full bg-primary text-white text-lg text-center rounded-t-lg p-2">
-                  Primeiro nome
-                </span>
-                <span className="block w-full bg-base-200 shadow-lg py-4 px-2 text-center">
-                  {handleShowValue(result.sintese.reg.primeiro_nome)}
-                </span>
-              </li>
-              <li>
-                <span className="block w-full bg-primary text-white text-lg text-center rounded-t-lg p-2">
-                  Sobrenome
-                </span>
-                <span className="block w-full bg-base-200 shadow-lg py-4 px-2 text-center">
-                  {handleShowValue(result.sintese.reg.ultimo_nome)}
-                </span>
-              </li>
-              <li>
-                <span className="block w-full bg-primary text-white text-lg text-center rounded-t-lg p-2">
-                  Gênero
-                </span>
-                <span className="block w-full bg-base-200 shadow-lg py-4 px-2 text-center">
-                  {handleShowValue(result.sintese.reg.sexo)}
-                </span>
-              </li>
-              <li>
-                <span className="block w-full bg-primary text-white text-lg text-center rounded-t-lg p-2">
-                  Data de nascimento:
-                </span>
-                <span className="block w-full bg-base-200 shadow-lg py-4 px-2 text-center">
-                  {handleShowValue(result.sintese.reg.nascimento)}
-                </span>
-              </li>
-              <li>
-                <span className="block w-full bg-primary text-white text-lg text-center rounded-t-lg p-2">
-                  Nome da mãe:
-                </span>
-                <span className="block w-full bg-base-200 shadow-lg py-4 px-2 text-center">
-                  {handleShowValue(result.sintese.reg.mae)}
-                </span>
-              </li>
-              <li>
-                <span className="block w-full bg-primary text-white text-lg text-center rounded-t-lg p-2">
-                  Nome do pai:
-                </span>
-                <span className="block w-full bg-base-200 shadow-lg py-4 px-2 text-center">
-                  {handleShowValue(result.sintese.reg.pai)}
-                </span>
-              </li>
-              <li>
-                <span className="block w-full bg-primary text-white text-lg text-center rounded-t-lg p-2">
-                  Estado civil:
-                </span>
-                <span className="block w-full bg-base-200 shadow-lg py-4 px-2 text-center">
-                  {handleShowValue(result.sintese.reg.estado_civil)}
-                </span>
-              </li>
-              <li>
-                <span className="block w-full bg-primary text-white text-lg text-center rounded-t-lg p-2 text-center">
-                  RG:
-                </span>
-                <span className="block w-full bg-base-200 shadow-lg py-4 px-2 text-center">
-                  {handleShowValue(result.sintese.reg.rg)}
-                </span>
-              </li>
-              <li>
-                <span className="block w-full bg-primary text-white text-lg text-center rounded-t-lg p-2">
-                  Nacionalidade:
-                </span>
-                <span className="block w-full bg-base-200 shadow-lg py-4 px-2 text-center">
-                  {handleShowValue(result.sintese.reg.nacionalidade)}
-                </span>
-              </li>
-              <li>
-                <span className="block w-full bg-primary text-white text-lg text-center rounded-t-lg p-2">
-                  Título de eleitor:
-                </span>
-                <span className="block w-full bg-base-200 shadow-lg py-4 px-2 text-center">
-                  {handleShowValue(result.sintese.reg.titulo_eleitor)}
-                </span>
-              </li>
-            </ul>
+                <div className="flex flex-col gap-2">
+                  {result.parentes.filhos.qtd &&
+                    result.parentes.filhos.registro.length > 0 &&
+                    result.parentes.filhos.registro.map((filho, i: number) => {
+                      return (
+                        <div key={i}>
+                          <span className="">{`${filho.nome} - `}</span>
+                          <span className="font-semibold">{`Nascido em ${filho.nascimento}`}</span>
+                        </div>
+                      );
+                    })}
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
